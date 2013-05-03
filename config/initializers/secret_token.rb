de0e934b69a5e8d11861f8a0969422055e6b8df9
root <root@malpraxpc.(none)>
2013-05-03 22:23:07 +0800

    pertamabuat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wandrr::Application.config.secret_key_base = '525bb776453601001a1a6053d88c7a899803165de004633b449f06907c6a3d481cdfaa28d8da0aa9d36f41432728d11c4063281453460540e23da0dd2b1a0ac8'
